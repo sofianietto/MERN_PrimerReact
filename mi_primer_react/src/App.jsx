e9b0cdd1e18475156ae4b07c3d8fef929295a75c
Sofia Nieto <sofialenieto12@gmail.com>
2024-02-29 19:01:47 -0300

    trabajo mi primer react con jsx

---
+++ mi_primer_react/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  return(
+    <>
+    <h1>Hola Dojo</h1>
+    <h2>Things I need to do:</h2>
+    <ul>
+      <li>Learn react</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
